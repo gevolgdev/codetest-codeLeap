fd9f1c6d4b8315893025d7e891933291a8f07fb1
Pinelli <ricardosilvapinelli@gmail.com>
2023-04-24 19:27:32 -0300

    Replaced Slices

---
+++ src/redux/reducers/userNameSlice.ts
@@ -0,0 +1,24 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { StateProps } from '../../types/types';
+import { RootState } from '../RootState';
+
+const initialState: StateProps = {
+  name: '',
+  activeButton: false,
+}
+
+export const userNameSlice = createSlice({
+  name: 'userName',
+  initialState,
+  reducers: {
+    userNameReducer: (state, action: PayloadAction<string>) => {
+      state.name = action.payload;
+      state.activeButton = true;
+    }
+  }
+})
+
+export const { userNameReducer } = userNameSlice.actions;
+
+export const selectUserName = (state: RootState) => state.userName;
+export default userNameSlice.reducer;
